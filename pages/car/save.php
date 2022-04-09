245409bc0357705b463903c9c9288b89bc065b9d
Jamorn Pengsuay <jamorn07@gmail.com>
2022-04-09 14:03:34 +0700

    สี

--- pages/car/save.php
+++ pages/car/save.php
@@ -4,7 +4,20 @@
 use App\Model\Car;
 $carObj = new Car;
 
-$carObj->addcar($_REQUEST);
-
+// $carObj->addcar($_REQUEST);
+if ($_REQUEST['action']=='delete'){
+    $carObj->deleteBook($_REQUEST['id']);
+}
+elseif ($_REQUEST['action']=='edit'){
+    $book = $_REQUEST;
+    unset($book['action']);
+    $carObj->updateBook($book);
+}
+elseif ($_REQUEST['action']=='add'){
+    $book = $_REQUEST;
+    unset($book['action']);
+    unset($book['id']);
+    $carObj->addBook($book);
+}
 header('Location: index.php');
 ?>
